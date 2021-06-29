7e7fac70e01484e89d408cd753419276bc44ef05
sangho1130 <56675654+sangho1130@users.noreply.github.com>
2021-06-29 10:51:00 +0900

    Add files via upload

---
+++ 3.dimension_reductions/coregenes_tsne_human_hcl/coregenes_tsne_human_hcl.R
@@ -0,0 +1,74 @@
+library(Seurat)
+library(ggplot2)
+
+humanobj <- readRDS('../../human/hcl/tmp/human_hcl.harmony.Rds')
+head(humanobj@meta.data); nrow(humanobj@meta.data)
+summary(humanobj@meta.data$celltype_refined)
+
+conserved <- read.delim('../../2.genelists/stats_orthologous.4847genes.txt')
+head(conserved); nrow(conserved)
+
+
+library(Rtsne)
+library(plyr)
+library(RColorBrewer)
+
+for (seed in c(210524161:210524170)){
+  set.seed(seed)
+  
+  ### random sampling ###
+  rand_bc <- c()
+  for (ct in levels(humanobj@meta.data$celltype_refined)) {
+    tmp <- subset(humanobj, celltype_refined == ct)
+    use_bc <- sample(rownames(tmp@meta.data), size = round(nrow(tmp@meta.data)/10))
+    rand_bc <- append(rand_bc, use_bc)
+  }
+  #length(rand_bc)
+  
+  data_mini <- as.matrix(FetchData(humanobj, vars = as.character(conserved$HumanSymbol), cells = rand_bc, slot = 'data'))
+  label_mini <- humanobj@meta.data[rownames(data_mini), ]
+  head(label_mini)
+  
+  ### get tSNE coordinates ###
+  tsne <- Rtsne(data_mini, dims = 3, pca_scale = F)
+  scores <- as.data.frame(tsne$Y)
+  rownames(scores) <- rownames(data_mini)
+  colnames(scores) <- c('tSNE1', 'tSNE2', 'tSNE3')
+  scores$celltype_refined <- label_mini$celltype_refined
+  
+  getPalette <- colorRampPalette(brewer.pal(8, "Set2"))
+  plt <- ggplot(scores, aes(x = tSNE1, y = tSNE2, colour = celltype_refined)) + 
+    geom_point() + 
+    scale_colour_manual(name="Cell type", values = getPalette(length(levels(humanobj@meta.data$celltype_refined)))) + 
+    theme_bw() + 
+    theme(axis.text = element_text(colour = 'black'),
+          axis.ticks = element_line(colour = 'black'),
+          panel.grid = element_blank()) +
+    labs(title =  ''); plt
+  ggsave(paste0(c('./', seed, '.1-2.pdf'), collapse = ''), units = 'cm', width = 12.5, height = 8)
+  
+  plt <- ggplot(scores, aes(x = tSNE1, y = tSNE3, colour = celltype_refined)) + 
+    geom_point() + 
+    scale_colour_manual(name="Cell type", values = getPalette(length(levels(humanobj@meta.data$celltype_refined)))) + 
+    theme_bw() + 
+    theme(axis.text = element_text(colour = 'black'),
+          axis.ticks = element_line(colour = 'black'),
+          panel.grid = element_blank()) +
+    labs(title =  ''); plt
+  ggsave(paste0(c('./', seed, '.1-3.pdf'), collapse = ''), units = 'cm', width = 12.5, height = 8)
+  
+  plt <- ggplot(scores, aes(x = tSNE2, y = tSNE3, colour = celltype_refined)) + 
+    geom_point() + 
+    scale_colour_manual(name="Cell type", values = getPalette(length(levels(humanobj@meta.data$celltype_refined)))) + 
+    theme_bw() + 
+    theme(axis.text = element_text(colour = 'black'),
+          axis.ticks = element_line(colour = 'black'),
+          panel.grid = element_blank()) +
+    labs(title =  ''); plt
+  ggsave(paste0(c('./', seed, '.2-3.pdf'), collapse = ''), units = 'cm', width = 12.5, height = 8)
+  
+  write.table(data.frame(Barcode = rownames(scores), scores, check.names = F, check.rows = F), 
+              paste0(c('./', seed, '.txt'), collapse = ''), quote = F, sep = '\t', row.names = F, col.names = T)
+}
+
+
